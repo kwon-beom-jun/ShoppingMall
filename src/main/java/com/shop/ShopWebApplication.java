61c8a7c4d90378d8bc6b6301a2da4e6b50552174
kwon-beom-jun <qjawns0618@naver.com>
2023-12-13 00:33:52 +0900

    2023.12.13 LomBok, Junit Setting

--- src/main/java/com/shop/ShopWebApplication.java
+++ src/main/java/com/shop/ShopWebApplication.java
@@ -2,12 +2,25 @@ package com.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ShopWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopWebApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public UserDto HeString() {
+
+		UserDto userDto = new UserDto();
+		userDto.setName("Kwon");
+		userDto.setAge(30);
+
+		return userDto;
 	}
 
 }
