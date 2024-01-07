5ab909dfa3dc0eeaa4655476cf4eb9d37c04ecb1
kwon-beom-jun <qjawns0618@naver.com>
2024-01-07 15:20:10 +0900

    2024.1.7 Thymeleaf Attribute Test

---
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -0,0 +1,67 @@
+package com.shop.controller;
+
+import com.shop.service.ThymeleafExService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/thymeleaf")
+public class ThymeleafExController {
+
+    @Autowired
+    ThymeleafExService service;
+
+    // URL : /thymeleaf/ex-text
+    @GetMapping(value = "/ex-text")
+    public String thymeleafExText(Model model) {
+        model.addAttribute("data", "타임리프 예제 입니다.");
+        return "thymeleafEx/thymeleafExText";
+    }
+
+    // URL : /thymeleaf/ex-dto
+    @GetMapping(value = "/ex-dto")
+    public String thymeleafExDto(Model model) {
+        model.addAttribute("itemDto", service.createSampleDto());
+        return "thymeleafEx/thymeleafExDto";
+    }
+
+    // URL : /thymeleaf/ex-each
+    @GetMapping(value = "/ex-each")
+    public String thymeleafExEach(Model model) {
+        model.addAttribute("itemDtoList", service.createSampleDtoList());
+        return "thymeleafEx/thymeleafExEach";
+    }
+
+    // URL : /thymeleaf/ex-if
+    @GetMapping(value = "/ex-if")
+    public String thymeleafExIf(Model model) {
+        model.addAttribute("itemDtoList", service.createSampleDtoList());
+        return "thymeleafEx/thymeleafExIf";
+    }
+
+    // URL : /thymeleaf/ex-switch
+    @GetMapping(value = "/ex-switch")
+    public String thymeleafExSwitch(Model model) {
+        model.addAttribute("itemDtoList", service.createSampleDtoList());
+        return "thymeleafEx/thymeleafExSwitch";
+    }
+
+    // URL : /thymeleaf/ex-href
+    @GetMapping(value = "/ex-href")
+    public String thymeleafExHref(Model model) {
+        return "thymeleafEx/thymeleafExHref";
+    }
+
+    // URL : /thymeleaf/ex-href-param
+    // 전달했던 매개 변수와 같은 이름의 String 변수 param1, param2를 파라미터로 설정하면 자동으로 데이터가 바인딩
+    @GetMapping(value = "/ex-href-param")
+    public String thymeleafExHrefParam(String param1, String param2, Model model) {
+        model.addAttribute("param1", param1);
+        model.addAttribute("param2", param2);
+        return "thymeleafEx/thymeleafExHrefParam";
+    }
+
+}
