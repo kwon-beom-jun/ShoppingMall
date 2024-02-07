daef1a6d52e9b5525d499cb6a5e471c0e5497c04
kwon-beom-jun <qjawns0618@naver.com>
2024-02-08 02:44:34 +0900

    Fix Missing Parts

--- shop-api/src/test/java/com/shop/ShopWebApplicationTests.java
+++ shop-api/src/test/java/com/shop/ShopWebApplicationTests.java
@@ -2,8 +2,10 @@ package com.shop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ShopWebApplicationTests {
 
 	@Test
