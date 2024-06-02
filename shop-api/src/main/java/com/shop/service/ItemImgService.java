f24887cabcd0cdb92ca44be6791562fe635e92e8
kwon-beom-jun <qjawns0618@naver.com>
2024-06-03 00:02:43 +0900

    Implement file upload service

--- shop-api/src/main/java/com/shop/service/ItemImgService.java
+++ shop-api/src/main/java/com/shop/service/ItemImgService.java
@@ -1,7 +1,55 @@
 package com.shop.service;
 
+import com.shop.entity.ItemImg;
+import com.shop.repository.ItemImgRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import org.thymeleaf.util.StringUtils;
+
 /**
  * FIXME : 추후 클라우드 파일서버 고려
  */
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class ItemImgService {
+
+    @Value("${itemImgLocation}")
+    private String itemImgLocation;
+
+    private final ItemImgRepository itemImgRepository;
+
+    private final FileService fileService;
+
+    public void saveItemImg(ItemImg itemImg, MultipartFile itemImgFile) throws Exception{
+        String oriImgName = itemImgFile.getOriginalFilename();
+        String imgName = "";
+        String imgUrl = "";
+
+        // 파일 업로드
+        if(!StringUtils.isEmpty(oriImgName)){
+            // Argument : 저장할 경로, 파일의 이름(경로 포함), 파일의 바이트 배열
+            // return : 로컬에 저장된 파일 이름(UUID + 파일 이름)
+            imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
+            /*
+                - 저장할 상품 이미지를 불러올 경로 설정
+                - 외부 리소스를 불러오는 urlPatterns(WebMvcConfig 클래스에서 "/images/**"를 설정)
+                - 프로퍼티에서 설정한 uploadPath 값 아래 item 폴더에 이미지를 저장하므로
+                  상품 이미지를 불러오는 경로로 "/images/item/"를 붙여줌
+             */
+            imgUrl = "/images/item/" + imgName;
+        }
+
+        // 상품 이미지 정보 저장
+        // Argument
+        //  - 실제 로컬에 저장된 상품 이미지 파일의 이름
+        //  - 업로드했던 상품 이미지 파일의 원래 이름
+        //  - 업로드 결과 로컬에 저장된 상품 이미지 파일을 불러오는 경로
+        itemImg.updateItemImg(oriImgName, imgName, imgUrl);
+        itemImgRepository.save(itemImg);
+    }
+
 }
